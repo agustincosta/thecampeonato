3f00a8dfa9d7bd5b92e257afee1e17ef6afa59c9
Agustín Costa <agustin.costa@pensur.com>
2022-07-09 12:27:36 -0300

    Always forget to run tests before pushing

--- Code/tests/test_img_funcs.py
+++ Code/tests/test_img_funcs.py
@@ -3,7 +3,6 @@ from ..img_funcs import ImageFunctions
 class TestImageFunctions:
     
     def test_readImageText(self):
-        imgfcts = ImageFunctions("Images/Tests/Screenshot.jpg")
+        imgfcts = ImageFunctions("Code/tests/support/Screenshot.jpg")
         text = imgfcts.readImageText()
-        print(text)
         assert text.splitlines()[0] == 'HOLA'
